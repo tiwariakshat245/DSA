3f9219ab08bbc2c35800b13cd1c0c4a6cd36ac7a
tiwariakshat245 <tiwariakshat245@gmail.com>
2026-09-06 14:29:11 +0530

    Time: 0 ms (100%), Space: 44.7 MB (37.02%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+         // person = rol
+	    // accounts = col
+	    int ans = Integer.MIN_VALUE;
+	    for (int person = 0; person<accounts.length; person ++) {
+	        // when you start a new col, take a new sum for that row 
+	        int sum =0;
+	        for(int account =0; account<accounts[person].length; account++) {
+	            sum += accounts[person] [account];
+	        }
+	        // now we have sum of accounts of person 
+	        // check with overall answer
+	        if(sum>ans) {
+	            ans=sum;
+	        }
+	        
+	    }
+	    return ans;
+        
+    }
+}
