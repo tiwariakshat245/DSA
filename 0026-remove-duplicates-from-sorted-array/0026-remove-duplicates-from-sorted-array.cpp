529d3ac321b7d258e4b836412fb2def25d3033f7
tiwariakshat245 <tiwariakshat245@gmail.com>
2026-09-12 09:03:15 +0530

    Time: 0 ms (100%), Space: 22.6 MB (80.62%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,33 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> ans;
+//         for(int i = 0; i < n; i++){
+//             // Only add the element if it's the first element or different from the previous one
+//             if(i == 0 || nums[i] != nums[i - 1]){
+//                 ans.push_back(nums[i]);
+//             }
+//         }
+//         int k = ans.size();
+//         for(int i = 0; i < k; i++){
+//             nums[i] = ans[i];
+//         }
+//         return k;
+//     }
+// };
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans;
-        for(int i = 0; i < n; i++){
-            // Only add the element if it's the first element or different from the previous one
-            if(i == 0 || nums[i] != nums[i - 1]){
-                ans.push_back(nums[i]);
+        if (nums.empty()) return 0;
+        
+        int k = 1; // Points to the position for the next unique element
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
             }
-        }
-        int k = ans.size();
-        for(int i = 0; i < k; i++){
-            nums[i] = ans[i];
         }
         return k;
     }
